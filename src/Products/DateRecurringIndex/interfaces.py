9c13b986803aca2ee3ce86805332e7454b30714e
Johannes Raggam <raggam-nl@adm.at>
2010-09-19 21:25:28 +0200

    remove unused

--- src/Products/DateRecurringIndex/interfaces.py
+++ src/Products/DateRecurringIndex/interfaces.py
@@ -20,24 +20,3 @@ class IRecurConfICal(IRecurConf):
 class IRecurConfTimeDelta(IRecurConf):
     """ Marker interface for timedelta recurring rules
     """
-
-
-#class IRRuleSet(Interface):
-    #""" Data Structure for dateutil.rset
-    #@ attr rrules: List of IRRule
-    #@ attr rdates: List of datetime instances to include
-    #@ attr exrules: List of IRRule for dates to exclude
-    #@ attr exdates: List of datetime instances to exclude
-    #"""
-#class IRRule(Interface):
-    #""" Data Structure for datetutil.rrule
-    #@ attr dtstart: The recurrence start. Besides being the base for the
-                    #recurrence, missing parameters in the final recurrence
-                    #instances will also be extracted from this date. If not
-                    #given, datetime.now() will be used instead.
-    #@ attr interval: The interval between each freq iteration. For example, when using YEARLY, an interval of 2 means once every two years, but with HOURLY, it means once every two hours. The default interval is 1.
-    #@ attr wkst: The week start day. Must be one of the MO, TU, WE constants, or an integer, specifying the first day of the week. This will affect recurrences based on weekly periods. The default week start is got from calendar.firstweekday(), and may be modified by calendar.setfirstweekday().
-    #@ attr count: How many occurrences will be generated.
-    #@ until until: If given, this must be a datetime instance, that will specify the limit of the recurrence. If a recurrence instance happens to be the same as the datetime instance given in the until keyword, this will be the last occurrence.
-    #...
-    #"""
