4eba18548babd106ef880d7e9690edacefff1919
Johannes Raggam <raggam-nl@adm.at>
2010-09-30 12:14:18 +0200

    timezone normalizing peeled out of IRecurrenceTimedelta and begun using it in IRecurrenceICal

--- src/Products/DateRecurringIndex/recurring.py
+++ src/Products/DateRecurringIndex/recurring.py
@@ -53,15 +53,40 @@ class RecurConfICal(RecurConf):
     implements(IRecurConfICal)
 
 
+def recurrence_normalize(date, delta=None, dstmode=DSTADJUST):
+    """Fixes invalid UTC offsets from recurrence calculations
+    @param date: datetime instance to normalize.
+    @param delta: integer or datetime.timedelta instance
+    @param dstmode: is either DSTADJUST, DSTKEEP or DSTAUTO. On DSTADJUST we have a
+            more human behaviour on daylight saving time changes: 8:00 on
+            day before spring dst-change plus 24h results in 8:00 day after
+            dst-change, which means in fact one hour less is added. On a
+            recurconf.recrule < 24h this will fail!
+            If DSTKEEP is selected, the time is added in its real hours, so
+            the above example results in 9:00 on day after dst-change.
+            DSTAUTO uses DSTADJUST for >=24h and DSTKEEP for < 24h
+            recurconf.recrule delta minutes.
+
+    """
+    if not date.tzinfo:
+        raise KeyError, u'Cannot normalize timezone naive dates'
+    assert(dst in [DSTADJUST, DSTKEEP, DSTAUTO])
+    if dst==DSTAUTO and delta<24*60:
+        dst = DSTKEEP
+    elif dst==DSTAUTO:
+        dst = DSTADJUST
+
+    if dst==DSTADJUST:
+        return date.replace(tzinfo=date.tzinfo.normalize(date).tzinfo)
+    else: # DSTKEEP
+        return date.tzinfo.normalize(date)
+
+
 @adapter(IRecurConfICal)
 @implementer(IRecurringSequence)
 def recurringSequenceICal(recurconf):
     """ Sequence of datetime objects from dateutil's recurrence rules
     """
-    rset = None
-    # the event's start date isn't explicitly set here. so if the recurrence
-    # rule's result does not include the start date, it won't be indexed
-    # but that's intentional and a corner case too
     if isinstance(recurconf.recrule, dateutil.rrule.rrule):
         rset = dateutil.rrule.rruleset()
         rset.rrule(recurconf.recrule)
@@ -73,14 +98,31 @@ def recurringSequenceICal(recurconf):
         # dtstart: optional used when no dtstart is in rfc2445 string
         #          recurconf.start is used which may be an event's endDate
         rset = dateutil.rrule.rrulestr(recurconf.recrule,
-                                       forceset=True,
-                                       dtstart=recurconf.start)
+                                       dtstart=recurconf.start,
+                                       forceset=True
+                                       # compatible=True
+                                       )
+    # RCF2445: Always include start date
+    rset.rdate(recurconf.start)
     # TODO: check dtstart and until/count for all rrule and exrules
     #       calculating without until/count takes loooong
-    if rset:
-        return list(rset)
-    else:
-        return None
+
+    # TODO: don't allwo timezone naive dates
+    # No DST normalizing for timezone naive dates
+    if not recurconf.start.tzinfo: return list(rset)
+
+    # Timezone normalizing and returning
+    before = None
+    for date in list(rset):
+        if not before:
+            # very first occurence - no need for normalizing
+            yield date
+        else:
+            delta = date - before
+            date = recurrence_normalize(date, delta, dst)
+            yield date
+            before = date
+
 
 @adapter(IRecurConfTimeDelta)
 @implementer(IRecurringSequence)
@@ -113,22 +155,13 @@ def recurringSequenceTimeDelta(recurconf):
         yield start
         return
 
-    assert(dst in [DSTADJUST, DSTKEEP, DSTAUTO])
-    if dst==DSTAUTO and delta<24*60:
-        dst = DSTKEEP
-    elif dst==DSTAUTO:
-        dst = DSTADJUST
-
-    until = pydt(until)
-    delta = datetime.timedelta(minutes=delta)
     yield start
     before = start
+    until = pydt(until)
+    delta = datetime.timedelta(minutes=delta)
     while True:
         after = before + delta
-        if dst==DSTADJUST:
-            after = after.replace(tzinfo=after.tzinfo.normalize(after).tzinfo)
-        else:
-            after = after.tzinfo.normalize(after)
+        after = recurrence_normalize(after, delta, dst)
         if utc(after) > utc(until):
             break
         yield after
