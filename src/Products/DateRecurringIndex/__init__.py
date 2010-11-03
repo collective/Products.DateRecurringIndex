d20965229470cc0e8d31b3fdabcb3c212e8d3223
Johannes Raggam <raggam-nl@adm.at>
2010-11-03 18:06:39 +0100

    well, initialize *is* needed by some zope initialization magic - thought it wasn't

---
+++ src/Products/DateRecurringIndex/__init__.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2007 by BlueDynamics Alliance, Klein & Partner KEG, Austria
+#
+# Zope Public License (ZPL)
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL). A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+
+__author__ = """Jens Klein <jens@bluedynamics.com>"""
+__docformat__ = 'plaintext'
+
+def initialize(context):
+    import index
+    context.registerClass(
+            index.DateRecurringIndex,
+            permission='Add Pluggable Index',
+            constructors=(index.manage_addDRIndexForm,
+                          index.manage_addDRIndex),
+            icon='www/index.gif',
+            visibility=None
+    )
