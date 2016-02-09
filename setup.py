3253ed1320ba979449eb968347d48743e54964f4
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:45:45 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     keywords='zope zope2 index catalog date recurring',
