87937d90cb3d65801ebc2c187b9b8bab68d61faa
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-07-05 22:28:31 +0200

    chore: update trove classifiers

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
     keywords="zope zope2 index catalog date recurring",
